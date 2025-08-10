9ed999a82e6915f8b5be6a7d6ba142071f6d1c5c
zhenggengqiang <getian233@163.com>
2025-08-10 22:34:36 +0800

    uv init

---
+++ src/mian/common/langChain_init.py
@@ -0,0 +1,19 @@
+from langchain_ollama import ChatOllama
+
+
+def ollama_chat_init(models: str):
+    """ChatOllama large language models."""
+    return ChatOllama(
+        base_url="http://127.0.0.1:11434/",
+        model=models,
+        reasoning=False,
+
+    )
+
+
+def ollama_llm_init(models: str):
+    """ollamaLlm large language models."""
+    return ChatOllama(
+        base_url="http://127.0.0.1:11434/",
+        model=models,
+    )
